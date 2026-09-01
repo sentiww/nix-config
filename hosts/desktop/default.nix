830ba53ef04c2840bf21c31d180eabc004e9e366
sentiww <wojciech.warwas01@gmail.com>
2026-09-01 17:26:45 +0200

    Remove WireGuard feature slice

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,11 +3,8 @@ _: {
     ./hardware-configuration.nix
     ../../features/remote-access/ssh-client.nix
     ../../features/remote-access/xrdp-server.nix
-    ../../features/networking/wireguard/nixos.nix
     ../../features/hardware/rtl-sdr.nix
   ];
-
-  hostWireguardIp = "10.0.0.2";
 
   networking.hostName = "nixos"; # Define your hostname.
   system.stateVersion = "25.05"; # State version of the NixOS release
--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -2,10 +2,7 @@ _: {
   imports = [
     ./hardware-configuration.nix
     ../../features/remote-access/ssh-client.nix
-    ../../features/networking/wireguard/nixos.nix
   ];
-
-  hostWireguardIp = "10.0.0.3";
 
   networking.hostName = "nixos"; # Define your hostname.
   system.stateVersion = "25.05"; # State version of the NixOS release
